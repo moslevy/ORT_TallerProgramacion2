70338770f8b0a65ef354c4c5a235607623e74253
Pablo Fernandez <pablo.fernandez.h@ypf.com>
2019-10-02 19:38:58 -0300

    clase 8 2da

---
+++ C08/DesafioTimer1.js
@@ -0,0 +1,15 @@
+/**
+ * DESAFIO 1
+ * Imprimir Hola Mundo por siempre, en intervalos de 1 segundo, incrementando 1 segundo cada vez.
+ * 
+ * La segunda vez demorará 2 segundos
+ * la tercera vez demorará 3 segundos
+ * 
+ * Ejem:
+ *  'Hola mundo 1'
+ *  'Hola mundo 2'
+ *  'Hola mundo 3'
+ *  
+ * Restricción
+ *  Solo se puede usar const (no let ni var)
+ */
---
+++ C08/DesafioTimer2.js
@@ -0,0 +1,21 @@
+/**
+ * Igual que el desafio 1, pero con los siguientes intervalos:
+ *  Imprimir "Hola mundo" 5 veces con retardo de 100ms
+ *  Imprimir "Hola mundo" 5 veces con retardo de 200ms
+ *  imprimir "Hola mundo" 5 veces con retardo de 300ms
+ *  ...
+ * Incluir en el mensaja el retardo impreso.
+ * Ejem.
+ *  Hola mundo 100
+ *  Hola mundo 100
+ *  Hola mundo 100
+ *  Hola mundo 100
+ *  Hola mundo 100
+ *  Hola mundo 200
+ *  Hola mundo 200
+ *  ...
+ * 
+ * Restricciones solo para avanzados:
+ * Solo usar setInterval, no usar Timeout
+ * Solo usar UNA instrucción If
+ * */
