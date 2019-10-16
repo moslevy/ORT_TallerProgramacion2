60452a74b0af6e9728aa0662f6a98fd51a6244f2
Pablo Fernandez <pablo.fernandez.h@ypf.com>
2019-10-16 20:35:49 -0300

    c09 02

--- C09/ejemploAwaitAsync.js
+++ C09/ejemploAwaitAsync.js
@@ -1,28 +1,52 @@
 /**
- * realiza el pedido de 🍕 que demora 200ml
+ * realiza el pedido de 🍕 que demora 200ms
  */
 function realizarPedido(){
-    
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve('🍕');
+        }, 200);
+    });
 }
 
 /**
- * realiza el pago 💸 online que demora 300ml
+ * realiza el pago 💸 online que demora 300ms
  */
 function realizaPagoOnline(){
-    
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('💸');
+        }, 300);
+    });
 }
 
 /**
  * 
- * realiza el envio a domicio 🚴
+ * realiza el envio a domicio 🚴 500mms
  */
 function realizaEnvioDomicilio(){
-    
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve('🚴');
+        }, 500);
+    });
 }
 
-async function compraOnline() {
-    
-}
+// function principal(){
+//     console.log(realizarPedido());
+//     console.log(realizaPagoOnline());
+//     console.log(realizaEnvioDomicilio());
+//     console.log('Termino de realizar toda las operaciones');
+// }
+
+ async function compraOnline() {
+    const pedidoTerminado = await realizarPedido();
+    const pagoTerminado = await realizaPagoOnline();
+    const envioTerminado = await realizaEnvioDomicilio();
+
+    console.log(`${pedidoTerminado} + ${pagoTerminado} + ${envioTerminado} = cliente :) `);
+
+ }
 
 compraOnline();
 
--- C09/server.js
+++ C09/server.js
@@ -1,1 +1,18 @@
 // web server con los modulos nativos de nodejs
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response)=> {
+    if(request.url == '/'){
+        response.write('Hola mundo');
+        response.end();
+    }
+
+    if(request.url == '/api/inventors'){
+        response.write(fs.readFileSync('./inventors.json','utf-8'));
+        response.end();
+    }
+});
+
+server.listen(3000);
+console.log('listen port 3000...');
